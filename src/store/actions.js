e192c0f2edb4c58e1418aac001f5889107d2f738
aimer1988 <aimer19881903@gmail.com>
2018-07-19 17:48:49 +0300

    project structure

---
+++ src/store/actions.js
@@ -0,0 +1,5 @@
+import mutation_types from './mutation_types';
+
+const actions = {};
+
+export default actions;
---
+++ src/store/mutations.js
@@ -0,0 +1,5 @@
+import mutation_types from './mutation_types';
+
+const mutations = {};
+
+export default mutations;
