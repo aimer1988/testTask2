ebff6a0a7d2fae1d0bf1448b1ad639adb3997059
aimer1988 <aimer19881903@gmail.com>
2018-07-19 17:40:07 +0300

    boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+require('webpack');
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    // eslint-disable-next-line
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-plain-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ]
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1000
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      // eslint-disable-next-line
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      // eslint-disable-next-line
+      '@store': path.resolve(__dirname, 'src/store')
+    }
+  }
+};
